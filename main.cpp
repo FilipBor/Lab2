fca243ac922b9ad1646a11dff0e02537453c5774
Filip Borecki <filibor847@student.polsl.pl>
2020-10-15 14:04:47 +0200

    Reading and creating files

--- main.cpp
+++ main.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int fibonacci_recursive(int index){
-    if (index < 2){
-        return index;
+int main(){
+
+    std::ofstream file;
+    file.open ("file.txt");
+    file << true;
+    file.close();
+
+
+    std::ifstream new_file("file.txt");
+
+    if (new_file.is_open())
+    {
+        std::string line;
+
+        while(getline(new_file, line))
+        {
+            std::cout << line << std::endl;;
+        }
+        new_file.close();
     }
-    return fibonacci_recursive(index-2) + fibonacci_recursive(index - 1);
-}
-int read_int(){
-    std::cout <<"Please introduce a number\n";
+    else{
+        std::cout <<"There was a problem opening the file";
+    }
 
-    int n;
-    std::cin >> n;
-    return n;
-}
-int main()
-{
-  int index = read_int();
-
-  int result = fibonacci_recursive(index);
-
-  std::cout <<"The result is: "<< result << std::endl;
   return 0;
 }
